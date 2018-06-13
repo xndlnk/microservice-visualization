da905edd3523e86f35414cb48c26cd53666985c0
Andreas Blunk <andreas.blunk@maibornwolff.de>
2018-06-13 21:15:38 +0200

    more tests for converter

--- sources/microservice-system-analyzer/test/model/ModelConverterTest.ts
+++ sources/microservice-system-analyzer/test/model/ModelConverterTest.ts
@@ -1,14 +1,14 @@
 import { describe, it } from 'mocha'
 import { expect } from 'chai'
-import * as v1Model from '~/model/model'
-import * as v0Model from './modelClasses'
+import * as v1 from '~/model/model'
+import * as v0 from './modelClasses'
 import { ModelConverter } from '~/model/ModelConverter'
 
 /* tslint:disable:no-unused-expression */
 describe('model converter', function() {
 
   it('can convert services, exchanges, and the links between them', function() {
-    const system: v0Model.System = {
+    const system: v0.System = {
       name: 'S',
       services: [
         { name: 'A' },
@@ -25,29 +25,27 @@ describe('model converter', function() {
 
     const v1system = new ModelConverter().convertSystem(system)
 
-    console.log(JSON.stringify(v1system, null, 2))
+    const expectedv1system = new v1.System('S')
 
-    const expectedv1system = new v1Model.System('S')
-
-    const msA = new v1Model.Microservice('A')
-    const msB = new v1Model.Microservice('B')
-    const msD = new v1Model.Microservice('D')
+    const msA = new v1.Microservice('A')
+    const msB = new v1.Microservice('B')
+    const msD = new v1.Microservice('D')
     expectedv1system.getMicroservices().push(msA)
     expectedv1system.getMicroservices().push(msB)
     expectedv1system.getMicroservices().push(msD)
 
-    const exC = new v1Model.MessageExchange('C')
+    const exC = new v1.MessageExchange('C')
     expectedv1system.getMessageExchanges().push(exC)
 
-    expectedv1system.getInfoFlows().push(new v1Model.AsyncInfoFlow(msA, msB))
-    expectedv1system.getInfoFlows().push(new v1Model.AsyncInfoFlow(msB, exC))
-    expectedv1system.getInfoFlows().push(new v1Model.AsyncInfoFlow(exC, msD))
+    expectedv1system.getInfoFlows().push(new v1.AsyncInfoFlow(msA, msB))
+    expectedv1system.getInfoFlows().push(new v1.AsyncInfoFlow(msB, exC))
+    expectedv1system.getInfoFlows().push(new v1.AsyncInfoFlow(exC, msD))
 
     expect(v1system).to.deep.equal(expectedv1system)
   })
 
-  /*it('can convert properties', function() {
-    const system: System = {
+  it('can convert properties', function() {
+    const system: v0.System = {
       name: 'S',
       services: [
         {
@@ -59,27 +57,18 @@ describe('model converter', function() {
       ]
     }
 
-    const node = new ModelConverter().convertSystemToNode(system)
+    const v1system = new ModelConverter().convertSystem(system)
 
-    const expectedNode: Node = RawModelConverter.convertToNode({
-      name: 'S',
-      kind: 'system',
-      nodes: [
-        {
-          name: 'A',
-          kind: 'microservice',
-          props: {
-            p1: 'v1'
-          }
-        }
-      ]
-    })
+    const expectedv1system = new v1.System('S')
+    const msA = new v1.Microservice('A')
+    msA.getProperties().p1 = 'v1'
+    expectedv1system.getMicroservices().push(msA)
 
-    expect(node).to.deep.equal(expectedNode)
+    expect(v1system).to.deep.equal(expectedv1system)
   })
 
   it('can convert subsystems', function() {
-    const system: System = {
+    const system: v0.System = {
       name: 'S',
       services: [
         { name: 'A' }
@@ -101,82 +90,23 @@ describe('model converter', function() {
       ]
     }
 
-    const node = new ModelConverter().convertSystemToNode(system)
+    const v1system = new ModelConverter().convertSystem(system)
 
-    const expectedNode: Node = RawModelConverter.convertToNode({
-      name: 'S',
-      kind: 'system',
-      nodes: [
-        { name: 'A', kind: 'microservice' },
-        {
-          name: 'P',
-          kind: 'system',
-          nodes: [
-            { name: 'M', kind: 'microservice' },
-            { name: 'N', kind: 'microservice' }
-          ],
-          edges: [
-            { sourceId: 'M', targetId: 'N', props: { communicationType: 'async' } }
-          ]
-        }
-      ],
-      edges: [
-        { sourceId: 'A', targetId: 'M', props: { communicationType: 'async' } }
-      ]
-    })
+    const expectedv1system = new v1.System('S')
+    const msA = new v1.Microservice('A')
+    expectedv1system.getMicroservices().push(msA)
 
-    expect(node).to.deep.equal(expectedNode)
+    const sP = new v1.System('P')
+    expectedv1system.getSubSystems().push(sP)
+
+    const msM = new v1.Microservice('M')
+    const msN = new v1.Microservice('N')
+    sP.getMicroservices().push(msM)
+    sP.getMicroservices().push(msN)
+    sP.getInfoFlows().push(new v1.AsyncInfoFlow(msM, msN))
+
+    expectedv1system.getInfoFlows().push(new v1.AsyncInfoFlow(msA, msM))
+
+    expect(v1system).to.deep.equal(expectedv1system)
   })
-
-  it('can convert exchanges', function() {
-    const system: System = {
-      name: 'S',
-      services: [
-        { name: 'A' },
-        { name: 'exchange A' }
-      ]
-    }
-
-    const node = new ModelConverter().convertSystemToNode(system)
-
-    const expectedNode: Node = RawModelConverter.convertToNode({
-      name: 'S',
-      kind: 'system',
-      nodes: [
-        { name: 'A', kind: 'microservice' },
-        { name: 'A', kind: 'exchange' }
-      ]
-    })
-
-    expect(node).to.deep.equal(expectedNode)
-  })
-
-  it('can convert link between service and exchange', function() {
-    const system: System = {
-      name: 'S',
-      services: [
-        { name: 'A' },
-        { name: 'exchange A' }
-      ],
-      links: [
-        { sourceName: 'A', targetName: 'exchange A', communicationType: 'async' }
-      ]
-    }
-
-    const node = new ModelConverter().convertSystemToNode(system)
-
-    const expectedNode: Node = RawModelConverter.convertToNode({
-      name: 'S',
-      kind: 'system',
-      nodes: [
-        { name: 'A', kind: 'microservice' },
-        { name: 'A', kind: 'exchange' }
-      ],
-      edges: [
-        { sourceId: 'A', targetId: 'M', props: { communicationType: 'async' } }
-      ]
-    })
-
-    expect(node).to.deep.equal(expectedNode)
-  })*/
 })
