e66e318c2c3683cf87b6144697264798dbd23d28
Andreas Blunk <andreas.blunk@maibornwolff.de>
2018-08-16 17:43:31 +0200

    playing with events

--- sources/microservice-system-viewer/src/frontend/viewhelper/EventRegistrator.ts
+++ sources/microservice-system-viewer/src/frontend/viewhelper/EventRegistrator.ts
@@ -3,21 +3,55 @@ import { SvgEdgeHighlightning } from './SvgEdgeHighlightning'
 
 export class EventRegistrator {
 
+  private static shiftPressed: boolean = false
+
   /**
    * Initiates all necessary events
    */
   public static init(): void {
-    for (let node of this.getRelevantNodes().values()) {
-      node.addEventListener('click', ScrollHandler.handler)
-      node.addEventListener('mouseover', SvgEdgeHighlightning.markAsHighlightedHandler)
-      node.addEventListener('mouseout', SvgEdgeHighlightning.markAsNotHighlightedHandler)
+    for (let element of this.getEdges().values()) {
+      element.addEventListener('click', ScrollHandler.handler)
+      element.addEventListener('mouseover', SvgEdgeHighlightning.markAsHighlightedHandler)
+      element.addEventListener('mouseout', SvgEdgeHighlightning.markAsNotHighlightedHandler)
+    }
+
+    for (let element of this.getNodesWithLinks().values()) {
+      const cssHighlightClass: string = 'highlight'
+      element.addEventListener('mouseover', (e: MouseEvent) => {
+        if (EventRegistrator.shiftPressed) {
+          const parentNode = e.srcElement.parentElement.parentElement.parentElement
+          parentNode.classList.add(cssHighlightClass)
+        }
+      })
+      element.addEventListener('mouseout', (e: MouseEvent) => {
+        const parentNode = e.srcElement.parentElement.parentElement.parentElement
+        parentNode.classList.remove(cssHighlightClass)
+      })
+      element.addEventListener('click',
+        (event: Event) => {
+          if (EventRegistrator.shiftPressed) {
+            EventRegistrator.shiftPressed = false
+            console.log('focus!')
+          }
+        })
+    }
+
+    window.onkeydown = (ev: KeyboardEvent) => {
+      EventRegistrator.shiftPressed = ev.shiftKey
+      console.log(EventRegistrator.shiftPressed)
+    }
+
+    window.onkeyup = (ev: KeyboardEvent) => {
+      EventRegistrator.shiftPressed = ev.shiftKey
+      console.log(EventRegistrator.shiftPressed)
     }
   }
 
-  /**
-   * @returns {NodeListOf<Element>}
-   */
-  private static getRelevantNodes(): NodeListOf<Element> {
+  private static getEdges(): NodeListOf<Element> {
     return document.querySelectorAll('g.edge>polygon,g.edge>ellipse')
+  }
+
+  private static getNodesWithLinks(): NodeListOf<Element> {
+    return document.querySelectorAll('g.node>g>a>polygon') // ,g.node>polygon,g.node>text
   }
 }
