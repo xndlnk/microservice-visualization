3aa9cf3c9dd418cccef9fcb10e2d195a14650857
xndlnk <xndlnk@gmail.com>
2019-05-27 16:47:53 +0200

    fix: entrypoint for ssh

--- sources/acmi-analyzer/entrypoint-ssh.sh
+++ sources/acmi-analyzer/entrypoint-ssh.sh
@@ -3,9 +3,10 @@ SSH_DIR=/root/.ssh
 if [ ! -d $SSH_DIR ]
   then
     mkdir -p ${SSH_DIR}
-    echo -n $SSH_PRIVATE_KEY > ${SSH_DIR}/id_rsa
-    echo -n $SSH_KNOWN_HOSTS > ${SSH_DIR}/known_hosts
+    echo "$SSH_PRIVATE_KEY" > ${SSH_DIR}/id_rsa
+    echo "$SSH_KNOWN_HOSTS" > ${SSH_DIR}/known_hosts
     chmod 0700 ${SSH_DIR}/
     chmod 0600 ${SSH_DIR}/id_rsa
+    chmod 0600 ${SSH_DIR}/known_hosts
 fi
 ./entrypoint.sh
