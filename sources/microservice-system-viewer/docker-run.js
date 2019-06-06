f9af4ab8b0a5875e35515c353061f4f7470f4c38
xndlnk <xndlnk@gmail.com>
2019-06-06 15:19:53 +0200

    renamed viewer to tadis-ui

--- sources/microservice-system-viewer/docker-run.js
+++
@@ -1,5 +0,0 @@
-const execSync = require('child_process').execSync
-
-const cmd = `docker run -p 8080:8080 -d --env-file .env microservice-system-viewer:${process.env.npm_package_version}`
-console.log('executing: ' + cmd)
-execSync(cmd, {stdio: [0, 1, 2]})
--- sources/tadis-ui/docker-build.js
+++ sources/tadis-ui/docker-build.js
@@ -1,5 +1,5 @@
 const execSync = require('child_process').execSync
 
-const cmd = `docker build -t microservice-system-viewer:${process.env.npm_package_version} .`
+const cmd = `docker build -t ${process.env.npm_package_name}:${process.env.npm_package_version} .`
 console.log('executing: ' + cmd)
 execSync(cmd, {stdio: [0, 1, 2]})
